a41c953113e8706520281ecd73437300ff80542a
Ozzy Osborne <bardweller@gmail.com>
2023-09-11 15:11:32 -0400

    Update detect.go

--- detect.go
+++ detect.go
@@ -8,16 +8,6 @@ func Detect(context libcnb.DetectContext) (libcnb.DetectResult, error) {
 	return libcnb.DetectResult{
 		Pass: true,
 		Plans: []libcnb.BuildPlan{
-			{
-				Provides: []libcnb.BuildPlanProvide{
-					{Name: "ubi-java-helper"},
-				},
-			},
-			{
-				Requires: []libcnb.BuildPlanRequire{
-					{Name: "ubi-java-helper"},
-				},
-			},
 		},
 	}, nil
 }
